c3d86ca4fdf6b5e40fbb471901622d56dc9d0310
caitlin <caitlin.wink@gmail.com>
2021-03-27 11:18:37 -0700

    commit tree 8fa831db563b5ba22024b5101c0fb1dade33ef80

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(gurls, event){
+    let empty = []
+    for (let i=0; i < gurls.length; i++){
+        empty.push(`Thank you, ${gurls[i]}, for the wonderful ${event} gift!`);
+    }
+    return empty;
+}
+function countDown(integer){
+    while (integer >= 0){
+        console.log(integer--);
+    }
+    return integer;
+}
+
+// function countDown(integer){
+//     let numbs = 0;
+//     while (numbs < 11){
+//         console.log(numbs++);
+//     }
+//     return integer;
+// }
